4c89effc814ebb07dfee8d5b89d65ebacde8e3a1
Ana de Paula <anaspaula17@gmail.com>
2025-10-29 21:46:04 -0300

    feat: add config file and module (#2)

---
+++ config.py
@@ -0,0 +1,11 @@
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read("config.ini") 
+
+ENCODED_FILE_PATH = config["INPUT FILES"]["encoded_file_path"]  
+
+ZERO = config["CONSTANTS"]["zero"]  
+ONE = config["CONSTANTS"]["one"]
+SEPARATOR = "\t"
