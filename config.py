06e8e0bb4bcfed74c618343a8e5885facf5cc619
Ana de Paula <anaspaula17@gmail.com>
2025-10-29 22:51:43 -0300

    Calculate frequencies (#5)                                                                                                                                         
    * chore: add quadgrams frequencies file to config
    
    * feat: calculate frequencies of encrypted text
    
    * feat: add frequency tables

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ config = configparser.ConfigParser()
 config.read("config.ini") 
 
 ENCODED_FILE_PATH = config["INPUT FILES"]["encoded_file_path"]  
+QUADGRAMS_FREQUENCY_FILE_PATH = config["INPUT FILES"]["quadgrams_frequency_file_path"]  
 
 ZERO = config["CONSTANTS"]["zero"]  
 ONE = config["CONSTANTS"]["one"]
